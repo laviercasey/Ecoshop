713da34bc9800ba29454320d424a6310a39b5de8
laviercasey <laviercasey@gmail.com>
2026-03-29 22:54:31 +0300

    fix: убран дублирующий uses() в Pest для Unit/Models

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,5 +4,4 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 uses(TestCase::class, RefreshDatabase::class)->in('Feature');
-uses(TestCase::class, RefreshDatabase::class)->in('Unit/Models');
 uses(TestCase::class)->in('Unit');

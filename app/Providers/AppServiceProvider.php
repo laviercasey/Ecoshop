e8533d1eabc4e5f07d88fa7531549678496b989d
laviercasey <laviercasey@gmail.com>
2026-06-10 14:11:34 +0300

    perf: preventLazyLoading и индексы на внешние ключи

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,15 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void {}
 
-    public function boot(): void {}
+    public function boot(): void
+    {
+        Model::preventLazyLoading(! app()->isProduction());
+    }
 }
